b662e051eddb0f6098ad155cb7db954ce8b81372
chadkraus87 <127354484+chadkraus87@users.noreply.github.com>
2023-06-06 18:07:40 -0500

    Modified connection.js.

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: 'chadkraus87',
-  password: 'r0wDy712$',
-  database: 'employeetracker'
+  user: process.env.DB_USER,
+  password: process.env.DB_USER_PASSWORD,
+  database: process.env.DB_DATABASE
 });
 
 module.exports = connection.promise();
--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,8 @@
+DROP DATABASE IF EXISTS employee_tracker;
+CREATE DATABASE employee_tracker;
+
+USE employee_tracker;
+
 DROP TABLE IF EXISTS employee;
 DROP TABLE IF EXISTS role;
 DROP TABLE IF EXISTS department;
