a8e233ae5d3c396a197f54985be084dcd5f2f32d
chadkraus87 <127354484+chadkraus87@users.noreply.github.com>
2023-06-06 15:29:45 -0500

    Adjusted seeds, schema.

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,18 +2,20 @@
 INSERT INTO department (name) VALUES
   ('Sales'),
   ('Marketing'),
-  ('Finance');
+  ('Finance'),
+  ('Support');
 
 -- Roles
 INSERT INTO role (title, salary, department_id) VALUES
-  ('Sales Manager', 60000, 1),
-  ('Sales Representative', 40000, 1),
-  ('Marketing Coordinator', 35000, 2),
-  ('Finance Analyst', 50000, 3);
+  ('Sales Manager', 80000, 1),
+  ('Sales Representative', 65000, 1),
+  ('Marketing Coordinator', 50000, 2),
+  ('Finance Analyst', 60000, 3),
+  ('Customer Support Manager', 70000, 4);
 
 -- Employees
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
-  ('John', 'Doe', 1, NULL),
-  ('Jane', 'Smith', 2, 1),
-  ('Mike', 'Johnson', 2, 1),
-  ('Sarah', 'Williams', 3, 1);
+  ('Captain', 'America', 1, NULL),
+  ('Jean', 'Grey', 2, 1),
+  ('Doctor', 'Strange', 2, 1),
+  ('Wonder', 'Woman', 4, NULL);
